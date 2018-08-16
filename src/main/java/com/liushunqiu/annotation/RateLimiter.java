19de363905c9a2425324085eaac9e91198c3b092
liushunqiu <402349563@qq.com>
2018-08-16 17:20:32 +0800

    限流增加IP限制

--- src/main/java/com/liushunqiu/annotation/RateLimiter.java
+++ src/main/java/com/liushunqiu/annotation/RateLimiter.java
@@ -3,7 +3,7 @@ package com.liushunqiu.annotation;
 import java.lang.annotation.*;
 
 /**
- * 默认60秒内每个请求只能有100次
+ * 默认每秒内每个请求只能有1000次
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
@@ -13,9 +13,9 @@ public @interface RateLimiter {
 
     TimeType timeType() default TimeType.SECOND;
 
-    int limit() default 100;
+    int limit() default 1000;
 
-    int expired() default 60;
+    int expired() default 1;
 
     enum Type {
         //限制IP
